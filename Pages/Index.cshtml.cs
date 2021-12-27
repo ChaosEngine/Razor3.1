ef536a8fb7616d2112e3f576776f8db0680daf3a
ChaosEngine <andrzej.pauli@gmail.com>
2021-12-27 22:14:51 +0100

    some index page session demo

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Razor3._1.Model;
@@ -16,6 +17,8 @@ namespace Razor3_1.Pages
 
 		public int SessionCacheCount { get; set; }
 
+		public int SeenCount { get; set; }
+
 		public IndexModel(ILogger<IndexModel> logger, BloggingContext dbContext)
 		{
 			_logger = logger;
@@ -25,6 +28,19 @@ namespace Razor3_1.Pages
 		public void OnGet()
 		{
 			SessionCacheCount = _dbContext.SessionCache.Count();
+
+			string seen_value = HttpContext.Session.GetString("Seen");
+			if (int.TryParse(seen_value, out var seen))
+			{
+				SeenCount = seen;
+			}
+			else
+				seen = 0;
+
+			_logger.LogInformation($"Seen => {seen}");
+			seen++;
+
+			HttpContext.Session.SetString("Seen", seen.ToString());
 		}
 	}
 }
